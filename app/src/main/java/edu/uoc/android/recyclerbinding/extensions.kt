02a093d2e17f29d784dff24046320578e0192b5b
xexi81 <74619705+xexi81@users.noreply.github.com>
2020-11-19 13:26:00 +0100

    V5: toast como función de extensión de ViewHolder

--- app/src/main/java/edu/uoc/android/recyclerbinding/extensions.kt
+++ app/src/main/java/edu/uoc/android/recyclerbinding/extensions.kt
@@ -2,8 +2,14 @@ package edu.uoc.android.recyclerbinding
 
 import android.content.Context
 import android.widget.Toast
+import android.widget.Toast.makeText
 import androidx.recyclerview.widget.RecyclerView
 
 fun Context.toast(message: String, length: Int = Toast.LENGTH_SHORT) {
-    Toast.makeText(this, message, length).show()
+    makeText(this, message, length).show()
+}
+
+
+fun RecyclerView.ViewHolder.toast(message: String, length: Int = Toast.LENGTH_SHORT) {
+    makeText(this.itemView.context, message, length).show()
 }
